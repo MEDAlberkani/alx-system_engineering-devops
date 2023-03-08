2d69378f3b718e733550eca200879f0f6d79c9e4
Mohammed <ss1366242@gmail.com>
2023-03-08 06:47:42 -0800

    101-wildcmp.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Entry point
+ * Description: Natural square root
+ * @n: Integer
+ * Return: -1 if negative n if 0 or 1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int x = _sqrt_recursion(n - 1) + 1;
+
+		if (x * x > n)
+		{
+			return (x - 1);
+		}
+		else if (x * x == n)
+		{
+			return (x);
+		}
+		else
+		{
+			return (_sqrt_recursion(n - 1) + 1);
+		}
+	}
+}
