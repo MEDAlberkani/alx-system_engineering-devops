57c529508e7cd02f128164424a34968c1629dce8
Mohammed <jamalbero18@gmail.com>
2023-08-28 06:33:45 -0700

    All

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# installing flask using pip3 in puppet
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
